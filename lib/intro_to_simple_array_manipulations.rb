fb6c22b1e06d8c0efc789581936241e210d6d6ed
Karim Macknojia <alishahal@msn.com>
2020-02-18 04:30:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,9 +38,7 @@ def using_flatten(array(another_array))
   array.flatten
 end
 
-def using_delete(array,string)
-  array.delete("string")
-end
+
 
 def using_delete_at(array,index)
   array.delete_at(index)
